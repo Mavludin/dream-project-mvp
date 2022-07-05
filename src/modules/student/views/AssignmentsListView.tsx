c3f49ea955384de9834541a10c71e104a7bc44eb
g322h <g2203h@gmail.com>
2022-07-05 15:43:07 +0300

    feat(filters): add antdeaign dropdown

--- src/modules/student/views/AssignmentsListView.tsx
+++ src/modules/student/views/AssignmentsListView.tsx
@@ -1,107 +1,66 @@
-import {
-  FilterOutlined,
-  BarChartOutlined,
-  StockOutlined,
-} from '@ant-design/icons';
-import { List, Space } from 'antd';
-import React, { useState } from 'react';
-import { AssigmentsData, Difficulty, FILTER_METHODS } from '../models';
-import styles from './AssignmentsListView.module.css';
+import { BarChartOutlined, StockOutlined } from "@ant-design/icons";
+import { List, Space } from "antd";
+import React, { useState } from "react";
+import { AssigmentListFilters } from "../components/AssigmentsListFilters/AssigmentListFilters";
+import { AssigmentsData } from "../models";
+import styles from "./AssignmentsListView.module.css";
 
 const IconText = ({ icon, text }: { icon: React.FC; text: string }) => (
-  <Space>
-    {React.createElement(icon)}
-    {text}
-  </Space>
+   <Space>
+      {React.createElement(icon)}
+      {text}
+   </Space>
 );
 
 type Props = {
-  assignmentsData: AssigmentsData[];
+   assignmentsData: AssigmentsData[];
 };
 
 export const AssignmentsListView = ({ assignmentsData }: Props) => {
-  const [filteredData, setFilteredData] = useState<AssigmentsData[]>([]);
+   const [filteredData, setFilteredData] = useState<AssigmentsData[]>([]);
 
-  const filterDifficulty = (difficulty: Difficulty) => {
-    const easy = assignmentsData.filter(
-      (item) => item.difficulty === difficulty,
-    );
+   const finalData = filteredData.length ? filteredData : assignmentsData;
 
-    setFilteredData(easy);
-  };
-
-  const resetFilterList = () => setFilteredData([]);
-
-  const finalData = filteredData.length ? filteredData : assignmentsData;
-
-  return (
-    <div className={styles.assignList}>
-      <div className={styles.title}>
-        <h1>Список задач</h1>
-        <div className={styles.filter}>
-          <FilterOutlined />
-          <ul>
-            <li>
-              <button onClick={resetFilterList}>Показать все</button>
-            </li>
-            {FILTER_METHODS.map((item) => (
-              <>
-                <li key={item.id}>
-                  <button>{item.name}</button>
-                </li>
-                {item.children && (
-                  <ul className={styles.subMenu}>
-                    {item.children.map((childItem) => (
-                      <li>
-                        <button
-                          onClick={() => filterDifficulty(childItem.type)}
-                        >
-                          {childItem.name}
-                        </button>
-                      </li>
-                    ))}
-                  </ul>
-                )}
-              </>
-            ))}
-          </ul>
-        </div>
-      </div>
-
-      <List
-        itemLayout="vertical"
-        size="large"
-        pagination={{
-          pageSize: 5,
-        }}
-        dataSource={finalData}
-        renderItem={(item, index) => (
-          <List.Item
-            className={styles.item}
-            key={item.id}
-            actions={[
-              <IconText
-                icon={BarChartOutlined}
-                text="Оценка: --"
-                key="list-vertical-star-o"
-              />,
-            ]}
-          >
-            <List.Item.Meta
-              title={(
-                <a href="/">
-                  {index + 1}
-                  .
-                  {item.name}
-                </a>
-              )}
+   return (
+      <div className={styles.assignList}>
+         <div className={styles.title}>
+            <h1>Список задач</h1>
+            <AssigmentListFilters
+               assignmentsData={assignmentsData}
+               setFilteredData={setFilteredData}
             />
-            <StockOutlined />
-            {' '}
-            {item.difficulty}
-          </List.Item>
-        )}
-      />
-    </div>
-  );
+         </div>
+
+         <List
+            itemLayout="vertical"
+            size="large"
+            pagination={{
+               pageSize: 5,
+            }}
+            dataSource={finalData}
+            renderItem={(item, index) => (
+               <List.Item
+                  className={styles.item}
+                  key={item.id}
+                  actions={[
+                     <IconText
+                        icon={BarChartOutlined}
+                        text="Оценка: --"
+                        key="list-vertical-star-o"
+                     />,
+                  ]}
+               >
+                  <List.Item.Meta
+                     title={
+                        <a href="/">
+                           {index + 1}.{item.name}
+                        </a>
+                     }
+                  />
+                  <StockOutlined /> {item.difficulty}
+               </List.Item>
+            )}
+         />
+      </div>
+   );
 };
