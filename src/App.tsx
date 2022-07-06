384f9fc9741c153818adf682baa8b578084b7bc8
rustamgadjiev <rustamgadjiev@list.ru>
2022-07-06 15:16:27 +0300

    feat(auth): keep user logged in

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,45 @@
-import './App.css';
-import { useEffect, useState } from 'react';
-import { AssigmentsData } from './models';
-import { AssignmentsListView } from './modules/student/views/AssignmentsListView';
-import { AuthView } from './modules/auth/views/AuthView';
+import "./App.css";
+import { useEffect, useState } from "react";
+import { Navigate, Route, Routes, useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { AssigmentsData } from "./models";
+import { AssignmentsListView } from "./modules/student/views/AssignmentsListView";
+import { AuthView } from "./modules/auth/views/AuthView";
+import { selectIsLoggedIn } from "./store/slices/auth";
 
 function App() {
   const [assignmentsData, setAssignmentsData] = useState<AssigmentsData[]>([]);
 
+  const navigate = useNavigate();
+
+  const isLoggedIn = useSelector(selectIsLoggedIn);
+
   useEffect(() => {
-    fetch('/api/assignments')
+    fetch("/api/assignments")
       .then((res) => res.json())
       .then((res) => setAssignmentsData(res.data));
   }, []);
 
+  useEffect(() => {
+    if (isLoggedIn) {
+      navigate("/student");
+    } else {
+      navigate("/auth");
+    }
+  }, [isLoggedIn]);
+
   return (
     <div className="App">
-      {/* <AssignmentsListView assignmentsData={assignmentsData} /> */}
-      <AuthView />
+      <Routes>
+        <Route
+          path="/auth"
+          element={<AuthView />}
+        />
+        <Route
+          path="/student"
+          element={<AssignmentsListView assignmentsData={assignmentsData} />}
+        />
+      </Routes>
     </div>
   );
 }
