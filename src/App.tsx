2b4f7d4039e981c82e7a4507b4af4dd5c90ea5db
rustamgadjiev <rustamgadjiev@list.ru>
2022-07-06 22:09:04 +0300

    fix(auth): moved routes to separate file

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,10 @@
 import './App.css';
 import { useEffect, useState } from 'react';
-import { Navigate, Route, Routes } from 'react-router-dom';
-import { useSelector } from 'react-redux';
 import { AssigmentsData } from './models';
-import { AssignmentsListView } from './modules/student/views/AssignmentsListView';
-import { AuthView } from './modules/auth/views/AuthView';
-import { selectIsLoggedIn } from './store/slices/auth';
+import { AppRoutes } from './AppRoutes';
 
 function App() {
   const [assignmentsData, setAssignmentsData] = useState<AssigmentsData[]>([]);
-
-  const isLoggedIn = useSelector(selectIsLoggedIn);
 
   useEffect(() => {
     fetch('/api/assignments')
@@ -20,23 +14,7 @@ function App() {
 
   return (
     <div className="App">
-      <Routes>
-        <Route path='/' element={isLoggedIn ? <Navigate to="/student" /> : <Navigate to="/auth" />} />
-        <Route
-          path="/auth"
-          element={isLoggedIn ? <Navigate to="/student" /> : <AuthView />}
-        />
-        <Route
-          path="/student"
-          element={
-            isLoggedIn ? (
-              <AssignmentsListView assignmentsData={assignmentsData} />
-            ) : (
-              <Navigate to="/auth" />
-            )
-          }
-        />
-      </Routes>
+      <AppRoutes assignmentsData={assignmentsData} />
     </div>
   );
 }
--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -1,1 +1,39 @@
-export {}
+import { useSelector } from 'react-redux';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import { AuthView } from './modules/auth/views/AuthView';
+import { AssigmentsData } from './modules/student/models';
+import { AssignmentsListView } from './modules/student/views/AssignmentsListView';
+import { selectIsLoggedIn } from './store/slices/auth';
+
+type Props = {
+    assignmentsData: AssigmentsData[];
+};
+
+export const AppRoutes = ({ assignmentsData }: Props) => {
+  const isLoggedIn = useSelector(selectIsLoggedIn);
+
+  return (
+    <Routes>
+      <Route
+        path="/"
+        element={
+          isLoggedIn ? <Navigate to="/student" /> : <Navigate to="/auth" />
+        }
+      />
+      <Route
+        path="/auth"
+        element={isLoggedIn ? <Navigate to="/student" /> : <AuthView />}
+      />
+      <Route
+        path="/student"
+        element={
+          isLoggedIn ? (
+            <AssignmentsListView assignmentsData={assignmentsData} />
+          ) : (
+            <Navigate to="/auth" />
+          )
+        }
+      />
+    </Routes>
+  );
+};
