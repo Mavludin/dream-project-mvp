74923241e678740c1726113159845a9707561de4
rustamgadjiev <rustamgadjiev@list.ru>
2022-07-07 15:44:43 +0300

    feat(noMatch): create design

---
+++ src/modules/noMatch/views/NoMatchView.tsx
@@ -0,0 +1,14 @@
+import { Link } from 'react-router-dom';
+import '../../../fonts/fonts.css';
+import s from './NoMatchView.module.css';
+
+export const NoMatchView = () => (
+  <div className={s.error}>
+    <h1 className={s.code}>404</h1>
+    <h2 className={s.text}>
+      not found
+      <span />
+    </h2>
+    <Link to="/">go home...</Link>
+  </div>
+);
