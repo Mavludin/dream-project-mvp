bfdbc59a7fc670a631be1705e127b391d29420e1
Mavludin <mavludin.a@develops.today>
2022-07-12 00:17:07 +0300

    feat(app-config): implement app config

---
+++ src/config/AppConfig.ts
@@ -0,0 +1,6 @@
+export default {
+  apiUrl: process.env.REACT_APP_API_URL,
+  contentfulUrl:
+    'https://graphql.contentful.com/content/v1/spaces/m61lhx9gr8sv/environments/master',
+  contentfulApiKey: process.env.REACT_APP_CONTENTFUL_API_KEY,
+};
--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,9 @@
 /// <reference types="react-scripts" />
+
+declare namespace NodeJS {
+  interface ProcessEnv {
+    // types of envs
+    REACT_APP_CONTENTFUL_API_KEY: string;
+    REACT_APP_API_URL: string;
+  }
+}
