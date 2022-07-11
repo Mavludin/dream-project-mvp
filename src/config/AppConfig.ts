4ab34595a351ac26667ddb5f38538edf127b173d
Mavludin <mavludin.a@develops.today>
2022-07-12 01:39:17 +0300

    fix(app-config): fix app config

--- src/config/AppConfig.ts
+++ src/config/AppConfig.ts
@@ -1,6 +1,6 @@
 export default {
   apiUrl: process.env.REACT_APP_API_URL,
   contentfulUrl:
-    'https://graphql.contentful.com/content/v1/spaces/m61lhx9gr8sv/environments/master',
+    'https://graphql.contentful.com/content/v1/spaces/yh0gtb7vyztv/environments/master/',
   contentfulApiKey: process.env.REACT_APP_CONTENTFUL_API_KEY,
 };
