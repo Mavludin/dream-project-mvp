e9902f0b6adc29a87b71fe4e451bc0d8e58c9f71
rustamgadjiev <rustamgadjiev@list.ru>
2022-07-13 02:08:16 +0300

    feat(teacher): add design

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -4,6 +4,7 @@ import { AuthView } from './modules/auth/views/AuthView';
 import { NoMatchView } from './modules/noMatch/views/NoMatchView';
 import { AssigmentsData } from './modules/student/models';
 import { AssignmentsListView } from './modules/student/views/AssignmentsListView';
+import { AssignmentListTeacherView } from './modules/teacher/views/AssignmentListTeacherView';
 import { selectIsLoggedIn } from './store/slices/auth';
 
 type Props = {
@@ -15,7 +16,7 @@ export const AppRoutes = ({ assignmentsData }: Props) => {
 
   return (
     <Routes>
-      <Route
+      {/* <Route
         path="/"
         element={
           isLoggedIn ? <Navigate to="/student" /> : <Navigate to="/auth" />
@@ -35,7 +36,13 @@ export const AppRoutes = ({ assignmentsData }: Props) => {
           )
         }
       />
-      <Route path="*" element={<NoMatchView />} />
+      <Route path="*" element={<NoMatchView />} /> */}
+      <Route
+        path="/teacher"
+        element={
+          <AssignmentListTeacherView assignmentsData={assignmentsData} />
+        }
+      />
     </Routes>
   );
 };
---
+++ src/modules/teacher/components/Task/Task.tsx
@@ -0,0 +1,23 @@
+// import { List } from 'antd';
+// import s from './Task.module.css';
+
+// export const Task = ({ item, index }) => (
+//   <List.Item className={s.item} key={item.id}>
+//     <div className={s.itemWrapper}>
+//       <List.Item.Meta
+//         title={
+//           <a href="/">
+//             {index + 1}.{item.name}
+//           </a>
+//         }
+//       />
+//       <StockOutlined /> {item.difficulty}
+//     </div>
+//     {isViewTask ? (
+//       <EyeTwoTone className={s.icon} />
+//     ) : (
+//       <EyeInvisibleOutlined className={s.icon} />
+//     )}
+//   </List.Item>
+// );
+export {};
