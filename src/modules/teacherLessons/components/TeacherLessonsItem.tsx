5716c29fcf0368aa40708d68fb0cea478050e6e8
g322h <g2203h@gmail.com>
2022-07-14 15:46:08 +0300

    feat(open-close-functional): add open/close lessons functional

--- src/modules/teacherLessons/components/TeacherLessonsItem.tsx
+++ src/modules/teacherLessons/components/TeacherLessonsItem.tsx
@@ -1,33 +1,97 @@
-import { DashOutlined, EyeOutlined } from '@ant-design/icons';
-import { List } from 'antd';
+import {
+  DashOutlined,
+  EyeInvisibleOutlined,
+  EyeOutlined,
+} from '@ant-design/icons';
+import { List, Spin } from 'antd';
+import React, { useEffect, useMemo, useState } from 'react';
 import { LessonItem } from '../../../models';
 import s from './TeacherLessonsItem.module.css';
 import { getLessonImageByType } from '../helpers/getLessonImageByType';
 
 type Props = {
   item: LessonItem;
+  openLessonsIds: string[];
 };
 
-export const TeacherLessonsItem = ({ item }: Props) => (
-  <List.Item>
-    <div className={s.item}>
-      <div className={s.info}>
-        <img src={getLessonImageByType(item.type)} alt="lesson" />
-        <div>
-          <h2>{item.title}</h2>
-          <p>{item.shortDescription}</p>
-        </div>
-      </div>
+export const TeacherLessonsItem = ({ item, openLessonsIds }: Props) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
-      <div className={s.buttons}>
-        <button className={s.icon}>
-          <EyeOutlined />
-        </button>
-        <span className={s.seporator} />
-        <button>
-          <DashOutlined className={s.dash} />
-        </button>
-      </div>
-    </div>
-  </List.Item>
-);
+  const isMatchIds = useMemo(
+    () => openLessonsIds.some((id) => id === item.sys.id),
+    [openLessonsIds, item],
+  );
+
+  useEffect(() => {
+    if (isMatchIds) {
+      setIsOpen(true);
+    }
+  }, [isMatchIds]);
+
+  const handleOpenCloseClick = async () => {
+    if (isOpen) {
+      setIsLoading(true);
+      const res = await fetch(`/api/open-lessons/${item.sys.id}`, {
+        method: 'DELETE',
+      });
+
+      if (res.ok) {
+        setIsOpen(false);
+        setIsLoading(false);
+      }
+    } else {
+      setIsLoading(true);
+      const res = await fetch('/api/open-lessons', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ lessonId: item.sys.id }),
+      });
+
+      if (res.ok) {
+        setIsOpen(true);
+        setIsLoading(false);
+      }
+    }
+  };
+  return (
+    <Spin tip="Loading..." spinning={isLoading}>
+      <List.Item>
+        <div className={s.item}>
+          <div className={s.info}>
+            <img src={getLessonImageByType(item.type)} alt="lesson" />
+            <div>
+              <h2>{item.title}</h2>
+              <p>{item.shortDescription}</p>
+            </div>
+          </div>
+
+          <div className={s.buttons}>
+            {isOpen ? (
+              <button
+                className={s.openIcon}
+                onClick={() => handleOpenCloseClick()}
+              >
+                <EyeOutlined />
+              </button>
+            ) : (
+              <button
+                className={s.closeIcon}
+                onClick={() => handleOpenCloseClick()}
+              >
+                <EyeInvisibleOutlined />
+              </button>
+            )}
+
+            <span className={s.seporator} />
+            <button>
+              <DashOutlined className={s.dash} />
+            </button>
+          </div>
+        </div>
+      </List.Item>
+    </Spin>
+  );
+};
