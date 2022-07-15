962d95ad9a45c74e8986a0456198019bc4b0ff0c
g322h <g2203h@gmail.com>
2022-07-15 14:09:08 +0300

    fix(teachers-lessons): add helpers

--- src/modules/teacherLessons/components/TeacherLessonsItem.tsx
+++ src/modules/teacherLessons/components/TeacherLessonsItem.tsx
@@ -2,49 +2,32 @@ import { DashOutlined, EyeOutlined } from '@ant-design/icons';
 import { List } from 'antd';
 import { LessonItem } from '../../../models';
 import s from './TeacherLessonsItem.module.css';
-import jsImg from '../assets/js.svg';
-import htmlImg from '../assets/html.svg';
-import reactImg from '../assets/react.svg';
-import cssImg from '../assets/css.svg';
+import { getLessonImageByType } from '../helpers/getLessonImageByType';
 
 type Props = {
   item: LessonItem;
 };
 
-export const TeacherLessonsItem = ({ item }: Props) => {
-  const getLessonImageByType = (lessonType: string) => {
-    switch (lessonType) {
-      case 'react':
-        return reactImg;
-      case 'html':
-        return htmlImg;
-      case 'css':
-        return cssImg;
-      default:
-        return jsImg;
-    }
-  };
-  return (
-    <List.Item>
-      <div className={s.item}>
-        <div className={s.info}>
-          <img src={getLessonImageByType(item.type)} alt="lesson" />
-          <div>
-            <h2>{item.title}</h2>
-            <p>{item.shortDescription}</p>
-          </div>
-        </div>
-
-        <div className={s.buttons}>
-          <button className={s.icon}>
-            <EyeOutlined />
-          </button>
-          <span className={s.seporator} />
-          <button>
-            <DashOutlined className={s.dash} />
-          </button>
+export const TeacherLessonsItem = ({ item }: Props) => (
+  <List.Item>
+    <div className={s.item}>
+      <div className={s.info}>
+        <img src={getLessonImageByType(item.type)} alt="lesson" />
+        <div>
+          <h2>{item.title}</h2>
+          <p>{item.shortDescription}</p>
         </div>
       </div>
-    </List.Item>
-  );
-};
+
+      <div className={s.buttons}>
+        <button className={s.icon}>
+          <EyeOutlined />
+        </button>
+        <span className={s.seporator} />
+        <button>
+          <DashOutlined className={s.dash} />
+        </button>
+      </div>
+    </div>
+  </List.Item>
+);
