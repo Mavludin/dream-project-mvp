1001e1cac476d8d41daf39959d58d5d4b0a7c896
rustamgadjiev <rustamgadjiev@list.ru>
2022-07-15 16:43:32 +0300

    feat(header): add drop-down menu

---
+++ src/components/DropDownMenu/DropDownMenu.tsx
@@ -0,0 +1,10 @@
+import s from './DropDownMenu.module.css';
+
+export const DropDownMenu = () => (
+  <div className={s.dropDown}>
+    <ul>
+      <li>Личный кабинет</li>
+      <li>Выход</li>
+    </ul>
+  </div>
+);
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,30 @@
-import React from 'react';
+import { useAppDispatch } from '../../store';
+import { logOut } from '../../store/slices/auth';
 import s from './Header.module.css';
 
 export function Header() {
+  const dispatch = useAppDispatch();
+
+  const handleExitClick = () => {
+    dispatch(logOut());
+  };
+
   return (
     <div className={s.header}>
       <div className={s.container}>
-        <button className={s.button}>Профиль</button>
+        <button className={s.button}>
+          Профиль
+          <div className={s.dropDown}>
+            <ul>
+              <li>
+                <button>Личный кабинет</button>
+              </li>
+              <li>
+                <button onClick={handleExitClick}>Выход</button>
+              </li>
+            </ul>
+          </div>
+        </button>
       </div>
     </div>
   );
