43c1392f717b49e7913d98bd20d2c6018626ec84
rustamgadjiev <rustamgadjiev@list.ru>
2022-07-28 15:11:07 +0300

    feat(studentLessons): add completed materials

--- src/modules/studentLessons/components/StudentLessonsFilters/StudentLessonsFilters.tsx
+++ src/modules/studentLessons/components/StudentLessonsFilters/StudentLessonsFilters.tsx
@@ -1,10 +1,19 @@
 import { useMemo } from 'react';
 import { FilterOutlined } from '@ant-design/icons';
-import { Menu, Dropdown, Space } from 'antd';
+import { Menu, Dropdown, Space, MenuProps } from 'antd';
 import s from './StudentLessonsFilters.module.css';
-import { FILTER_METHODS } from '../../models';
+import { FILTER_METHODS, Type } from '../../models';
+import { LessonItem } from '../../../../models';
 
-export const StudentLessonsFilters = () => {
+type Props = {
+  openLessons: LessonItem[];
+  setFilteredData: (arr: LessonItem[]) => void;
+};
+
+export const StudentLessonsFilters = ({
+  setFilteredData,
+  openLessons,
+}: Props) => {
   const filterItems = useMemo(
     () =>
       FILTER_METHODS.map((item) => ({
@@ -20,7 +29,31 @@ export const StudentLessonsFilters = () => {
     [],
   );
 
-  const menu = <Menu items={filterItems} />;
+  const resetFilterList = () => setFilteredData([]);
+
+  const filterType = (type: Type) => {
+    setFilteredData(openLessons.filter((item) => item.type === type));
+  };
+
+  const handleMenuFilters: MenuProps['onClick'] = (e) => {
+    if (e.key === '1') {
+      resetFilterList();
+    } else if (e.key === '2-1') {
+      console.log(1);
+    } else if (e.key === '2-2') {
+      console.log(2);
+    } else if (e.key === '3-1') {
+      filterType('html');
+    } else if (e.key === '3-2') {
+      filterType('css');
+    } else if (e.key === '3-3') {
+      filterType('js');
+    } else if (e.key === '3-4') {
+      filterType('react');
+    }
+  };
+
+  const menu = <Menu onClick={handleMenuFilters} items={filterItems} />;
   return (
     <div className={s.filter}>
       <FilterOutlined />
