8cc7d70785db32be840cb851e5c5cea2c1668ce6
rustamgadjiev <rustamgadjiev@list.ru>
2022-07-28 15:11:07 +0300

    feat(studentLessons): add completed materials

--- src/modules/studentLessons/components/StudentLessonsItem/StudentLessonsItem.tsx
+++ src/modules/studentLessons/components/StudentLessonsItem/StudentLessonsItem.tsx
@@ -1,5 +1,9 @@
 import { Card, List } from 'antd';
-import { EllipsisOutlined, MinusOutlined } from '@ant-design/icons';
+import {
+  CheckOutlined,
+  EllipsisOutlined,
+  MinusOutlined,
+} from '@ant-design/icons';
 import Meta from 'antd/lib/card/Meta';
 import { LessonItem } from '../../../../models';
 import s from './StudentLessonsItem.module.css';
@@ -7,26 +11,42 @@ import { getLessonImageByType } from '../../../../helpers/getLessonImageByType';
 
 type Props = {
   item: LessonItem;
+  readLessons: string[];
 };
 
-export const StudentLessonsItem = ({ item }: Props) => (
-  <List.Item>
-    <Card
-      bodyStyle={{
-        lineHeight: '15px',
-        marginBottom: '5px',
-        height: '84px',
-      }}
-      size="small"
-      actions={[
-        <MinusOutlined className={s.minus} />,
-        <EllipsisOutlined style={{ fontSize: '24px' }} key="ellipsis" />,
-      ]}
-    >
-      <div className={s.info}>
-        <img src={getLessonImageByType(item.type)} alt="lesson icon" />
-        <Meta title={item.title} description={item.shortDescription} />
-      </div>
-    </Card>
-  </List.Item>
-);
+export const StudentLessonsItem = ({ item, readLessons }: Props) => {
+  const isReading = readLessons.some((id) => item.sys.id === id);
+
+  return (
+    <List.Item>
+      <Card
+        className={isReading ? s.complete : ''}
+        bodyStyle={{
+          lineHeight: '15px',
+          marginBottom: '5px',
+          height: '84px',
+        }}
+        size="small"
+        actions={[
+          isReading ? (
+            <CheckOutlined style={{ fontSize: '24px', color: '#58B588' }} />
+          ) : (
+            <MinusOutlined className={s.minus} />
+          ),
+          <EllipsisOutlined
+            style={{
+              fontSize: '24px',
+              color: isReading ? '#58B588' : '',
+            }}
+            key="ellipsis"
+          />,
+        ]}
+      >
+        <div className={s.info}>
+          <img src={getLessonImageByType(item.type)} alt="lesson icon" />
+          <Meta title={item.title} description={item.shortDescription} />
+        </div>
+      </Card>
+    </List.Item>
+  );
+};
