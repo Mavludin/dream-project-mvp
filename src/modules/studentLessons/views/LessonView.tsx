083e2e24b50bb5039586d14b9a1d8200d75c83ef
Sey-Uki <abakarova.diana99@gmail.com>
2022-08-21 23:21:25 +0300

    feat(studentLessons): add style student lesson page

--- src/modules/studentLessons/views/LessonView.tsx
+++ src/modules/studentLessons/views/LessonView.tsx
@@ -4,6 +4,7 @@ import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
 import { useAppSelector } from '../../../store';
 import { lessonsGraphqlApi } from '../../../store/api/lessonsApi';
 import { selectLessons } from '../../../store/slices/lessons';
+import styles from './LessonView.module.css';
 
 export const LessonView = () => {
   const { lessonId } = useParams();
@@ -21,9 +22,14 @@ export const LessonView = () => {
   if (!lesson) return null;
 
   return (
-    <div>
-      МАТЕРИАЛ
+    <div className={styles.lessonView}>
       {documentToReactComponents(lesson?.description?.json)}
+
+      <div className={styles.btn}>
+        <button className={styles.nav}>Назад</button>
+        <button className={styles.read}>Прочитано</button>
+        <button className={styles.nav}>Далее</button>
+      </div>
     </div>
   );
 };
