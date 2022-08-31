de667e3abc7b099245667a49a66c03690a2b704a
REzoa1 <z_babaeva2000@mail.ru>
2022-08-31 20:11:27 +0300

    feat(apiUrl): add backend link

--- src/config/AppConfig.ts
+++ src/config/AppConfig.ts
@@ -1,6 +1,5 @@
 export default {
-  apiUrl: (process.env.REACT_APP_API_URL =
-    'https://dream-project-mvp-backend.vercel.app'),
+  apiUrl: process.env.REACT_APP_API_URL,
   contentfulUrl:
     'https://graphql.contentful.com/content/v1/spaces/yh0gtb7vyztv/environments/master/',
   contentfulApiKey: process.env.REACT_APP_CONTENTFUL_API_KEY,
