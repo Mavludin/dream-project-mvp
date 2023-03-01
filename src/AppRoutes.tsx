3949c1e6001546e099ed26f42a13277e7ffc83f7
SeyUki <abakarova.diana99@gmail.com>
2023-03-01 21:00:28 +0300

    fix(auth): divide routes with header and without

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -1,7 +1,4 @@
-import { useMemo } from 'react';
-import { Navigate, Route, Routes, useLocation } from 'react-router-dom';
-import { Header } from './components/Header/Header';
-import { getHomeRouteByUserType } from './helpers/getHomeRouteByUserType';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import { AuthView } from './modules/auth/views/AuthView';
 import { NoMatchView } from './modules/noMatch/views/NoMatchView';
 import { StudentAssignmentsView } from './modules/student/views/StudentAssignmentsView';
@@ -13,20 +10,11 @@ import { TeacherLessonView } from './modules/teacherLessons/views/TeacherLessonV
 import { TeacherLessons } from './modules/teacherLessons/views/TeacherLessons';
 import { useAppSelector } from './store';
 import { selectIsLoggedIn, selectUserType } from './store/slices/auth';
-import { selectLessonsCollection } from './store/slices/lessons';
-
-// const TEACHER_ROUTES = ['/teacher/assignments', '/teacher/lessons'];
-// const STUDENT_ROUTES = ['/student/assignments', '/student/lessons'];
+import { HeaderPage } from './components/Header/HeaderPage';
 
 export const AppRoutes = () => {
   const isLoggedIn = useAppSelector(selectIsLoggedIn);
   const userType = useAppSelector(selectUserType);
-
-  console.log(userType);
-
-  // const lessonsCollection = useAppSelector(selectLessonsCollection);
-
-  // const location = useLocation();
 
   const AUTH_ROUTE = isLoggedIn ? (
     <Navigate to={`/${userType}/assignments`} />
@@ -34,71 +22,48 @@ export const AppRoutes = () => {
     <AuthView />
   );
 
-  // const isTeacherRoute = useMemo(
-  //   () => TEACHER_ROUTES.some((route) => location.pathname === route),
-  //   [location.pathname],
-  // );
-  // const isStudentRoute = useMemo(
-  //   () => STUDENT_ROUTES.some((route) => location.pathname === route),
-  //   [location.pathname],
-  // );
-
-  // const lessonsRoutes = lessonsCollection.map(
-  //   (item) => `/${userType}/lessons/${item.sys.id}`,
-  // );
-
-  // const isLessonPage = useMemo(
-  //   () => lessonsRoutes.some((route) => location.pathname === route),
-  //   [location.pathname, lessonsRoutes],
-  // );
-
   return (
     <>
-      {/* {(userType === 'student' || userType === 'teacher') &&
-        (isStudentRoute || isTeacherRoute || isLessonPage) && <Header />} */}
-
-      {isLoggedIn && <Header />}
-
       <Routes>
-        <Route
-          path="/"
-          // element={getHomeRouteByUserType(userType, isLoggedIn)}
-          element={AUTH_ROUTE}
-        />
+        <Route index element={AUTH_ROUTE} />
         <Route path="/auth" element={AUTH_ROUTE} />
         {userType === 'student' && (
           <>
             <Route
-              path="/student/assignments"
-              element={<StudentAssignmentsView />}
-            />
-            <Route
               path="/student/assignments/:asgmtId"
               element={<TaskView />}
             />
-            <Route path="/student/lessons" element={<StudentLessons />} />
-            <Route
-              path="/student/lessons/:lessonId"
-              element={<StudentLessonView />}
-            />
           </>
         )}
-        {userType === 'teacher' && (
-          <>
-            <Route
-              path="/teacher/assignments"
-              element={<TeacherAssignmentsView />}
-            />
-            <Route path="/teacher/lessons" element={<TeacherLessons />} />
-            <Route
-              path="/teacher/lessons/:lessonId"
-              element={<TeacherLessonView />}
-            />
-          </>
-        )}
-        {/* {(isTeacherRoute || isStudentRoute) && (
-          <Route path={location.pathname} element={<Navigate to="/auth" />} />
-        )} */}
+
+        <Route path={userType} element={<HeaderPage />}>
+          {userType === 'student' && (
+            <>
+              <Route
+                path="/student/assignments"
+                element={<StudentAssignmentsView />}
+              />
+              <Route path="/student/lessons" element={<StudentLessons />} />
+              <Route
+                path="/student/lessons/:lessonId"
+                element={<StudentLessonView />}
+              />
+            </>
+          )}
+          {userType === 'teacher' && (
+            <>
+              <Route
+                path="/teacher/assignments"
+                element={<TeacherAssignmentsView />}
+              />
+              <Route path="/teacher/lessons" element={<TeacherLessons />} />
+              <Route
+                path="/teacher/lessons/:lessonId"
+                element={<TeacherLessonView />}
+              />
+            </>
+          )}
+        </Route>
         <Route path="*" element={<NoMatchView />} />
       </Routes>
     </>
