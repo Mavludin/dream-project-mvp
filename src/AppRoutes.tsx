1f7f04496981f8169ae52469f9a78926189db1b7
SeyUki <abakarova.diana99@gmail.com>
2023-03-06 13:02:59 +0300

    fix(auth): divide routes teacher and student

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -1,16 +1,12 @@
 import { Navigate, Route, Routes } from 'react-router-dom';
 import { AuthView } from './modules/auth/views/AuthView';
 import { NoMatchView } from './modules/noMatch/views/NoMatchView';
-import { StudentAssignmentsView } from './modules/student/views/StudentAssignmentsView';
 import { TaskView } from './modules/student/views/TaskView';
-import { StudentLessonView } from './modules/studentLessons/views/StudentLessonView';
-import { StudentLessons } from './modules/studentLessons/views/StudentLessons';
-import { TeacherAssignmentsView } from './modules/teacher/views/TeacherAssignmentsView';
-import { TeacherLessonView } from './modules/teacherLessons/views/TeacherLessonView';
-import { TeacherLessons } from './modules/teacherLessons/views/TeacherLessons';
 import { useAppSelector } from './store';
 import { selectIsLoggedIn, selectUserType } from './store/slices/auth';
-import { HeaderPage } from './components/Header/HeaderPage';
+import { PageWithHeader } from './components/Header/PageWithHeader';
+import { TeacherRoutes } from './TeacherRoutes';
+import { StudentRoutes } from './StudentRoutes';
 
 export const AppRoutes = () => {
   const isLoggedIn = useAppSelector(selectIsLoggedIn);
@@ -28,42 +24,14 @@ export const AppRoutes = () => {
         <Route index element={AUTH_ROUTE} />
         <Route path="/auth" element={AUTH_ROUTE} />
         {userType === 'student' && (
-          <>
-            <Route
-              path="/student/assignments/:asgmtId"
-              element={<TaskView />}
-            />
-          </>
+          <Route path="/student/assignments/:asgmtId" element={<TaskView />} />
         )}
 
-        <Route path={userType} element={<HeaderPage />}>
-          {userType === 'student' && (
-            <>
-              <Route
-                path="/student/assignments"
-                element={<StudentAssignmentsView />}
-              />
-              <Route path="/student/lessons" element={<StudentLessons />} />
-              <Route
-                path="/student/lessons/:lessonId"
-                element={<StudentLessonView />}
-              />
-            </>
-          )}
-          {userType === 'teacher' && (
-            <>
-              <Route
-                path="/teacher/assignments"
-                element={<TeacherAssignmentsView />}
-              />
-              <Route path="/teacher/lessons" element={<TeacherLessons />} />
-              <Route
-                path="/teacher/lessons/:lessonId"
-                element={<TeacherLessonView />}
-              />
-            </>
-          )}
+        <Route path={userType} element={<PageWithHeader />}>
+          {userType === 'student' && <StudentRoutes />}
+          {userType === 'teacher' && <TeacherRoutes />}
         </Route>
+
         <Route path="*" element={<NoMatchView />} />
       </Routes>
     </>
