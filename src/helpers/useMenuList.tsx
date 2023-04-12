63e07c4d682eb2cbd584e1143c5375509ae42284
REzoa1 <z_babaeva2000@mail.ru>
2023-04-12 17:53:48 +0300

    fix(filters): add useMenuList, wrap function in useCallback

---
+++ src/helpers/useMenuList.tsx
@@ -0,0 +1,28 @@
+import { Menu, MenuProps } from 'antd';
+import { ItemType } from 'antd/lib/menu/hooks/useItems';
+import type { MenuClickEventHandler } from 'rc-menu/lib/interface';
+import { useCallback, useState } from 'react';
+
+export const useMenuList = (
+  filterItems: ItemType[],
+  handleMenuFilters: MenuClickEventHandler = () => null,
+) => {
+  const [openKeys, setOpenKeys] = useState<string[]>([]);
+
+  const onOpenChange: MenuProps['onOpenChange'] = useCallback(
+    (keys: string[]) => {
+      const latestOpenKey = keys.find((key) => openKeys.indexOf(key) === -1);
+      setOpenKeys(latestOpenKey ? [latestOpenKey] : []);
+    },
+    [openKeys],
+  );
+
+  return (
+    <Menu
+      openKeys={openKeys}
+      onOpenChange={onOpenChange}
+      items={filterItems}
+      onClick={handleMenuFilters}
+    />
+  );
+};
